d4f33e49ff21aa7aba6ab0f2fa114fd0ba1f13a0
Sam Gammon <sam@momentum.io>
2017-04-02 09:16:26 -0700

    Initial commit
    - Basic component structure
    - Build and analysis tools
    - Initial Polymer work for momentum/bloombox#328, under issue
      momentum/bloombox#175

---
+++ README.md
@@ -0,0 +1,4 @@
+
+##&lt;bloombox-embedded-menu&gt;  [![Build Status](https://buildbot.hq.mm-corp.systems/jenkins/buildStatus/icon?job=Bloombox/elements/bloombox-embedded-menu)](https://buildbot.hq.mm-corp.systems/jenkins/job/Bloombox/elements/bloombox-embedded-menu)
+
+The `bloombox-embedded-menu` element provides UI for publicly embeddedable web menus.
---
+++ bloombox-embedded-menu.js
@@ -0,0 +1,58 @@
+
+/**
+ * `<bloombox-embedded-menu>`
+ */
+var BloomboxEmbeddedMenu = Polymer.BloomboxEmbeddedMenu = Polymer({
+  is: 'bloombox-embedded-menu',
+
+  properties: {
+    /**
+     * API key to authorize communications with the Bloombox APIs.
+     *
+     * @type {string}
+     */
+    apikey: {
+      type: String,
+      notify: true,
+      reflectToAttribute: true
+    },
+
+    /**
+     * Partner account ID we are displaying the menu for.
+     *
+     * @type {string}
+     */
+    partner: {
+      type: String,
+      notify: true,
+      reflectToAttribute: true
+    },
+
+    /**
+     * Location account ID we are displaying the menu for.
+     *
+     * @type {string}
+     */
+    location: {
+      type: String,
+      notify: true,
+      reflectToAttribute: true
+    }
+  },
+
+  listeners: [
+    "_widgetReady(apikey, partner, location)"
+  ],
+
+  /**
+   * Dispatched once this component has all requisite configuration
+   * to proceed and load data/render.
+   *
+   * @param {string} apikey API key to use for underlying public auth.
+   * @param {string} partner Partner account ID.
+   * @param {string} location Location ID to load menu for.
+   */
+  _widgetReady: function(apikey, partner, location) {
+    
+  }
+});
